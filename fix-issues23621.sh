e303de596042ce8337cf5440f6ff92a9234b24e8
王伟东 <weidong_wang@jit.com.cn>
2024-06-04 12:07:42 +0800

    修改patch文件的bug

--- fix-issues23621.sh
+++ fix-issues23621.sh
@@ -3,18 +3,25 @@ cat > feeds/packages/libs/libpfring/patches/999-fix-iss23621.patch << 'EOF'
 #https://github.com/openwrt/packages/issues/23621
 --- a/kernel/pf_ring.c
 +++ b/kernel/pf_ring.c
-@@ -5605,7 +5605,7 @@ static int packet_ring_bind(struct sock
- static int ring_bind(struct socket *sock, struct sockaddr *sa, int addr_len)
+@@ -5562,18 +5562,18 @@ static int ring_bind(struct socket *sock, struct sockaddr *sa, int addr_len)
  {
    struct sock *sk = sock->sk;
 -  char name[sizeof(sa->sa_data)+1];
 +  char name[sizeof(sa->sa_data_min)+1];
+ 
    debug_printk(2, "ring_bind() called\n");
-@@ -5617,7 +5617,7 @@ static int ring_bind(struct socket *sock
+ 
+   /*
+    * Check legality
+    */
+   if(addr_len != sizeof(struct sockaddr))
+     return(-EINVAL);
    if(sa->sa_family != PF_RING)
      return(-EINVAL);
+ 
 -  memcpy(name, sa->sa_data, sizeof(sa->sa_data));
 +  memcpy(name, sa->sa_data, sizeof(sa->sa_data_min));
+ 
    /* Add trailing zero if missing */
    name[sizeof(name)-1] = '\0';
 EOF
